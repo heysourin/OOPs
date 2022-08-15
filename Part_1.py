c1d4bcf663cceffc3e94af81575eb084355e34ef
Sourin <98945276+heysourin@users.noreply.github.com>
2022-08-15 22:43:48 +0530

    OOPs: attributes and __init__

---
+++ Part_1.py
@@ -0,0 +1,28 @@
+#! now, we dont want to hard code the attributes(instance attribute): __init__
+
+class Item:
+    def __init__(self, name: str, price: float, quantity=0):
+        #Run validations to the received arguments
+        assert price >=0, f"Price {price} is not grater than or equal to zero!"
+        assert quantity >=0, f"Price {quantity} is not grater than or equal to zero!"
+
+        #Assign to self object
+        print(f"An instance created, Name: {name}, Price: {price}, Qt: {quantity}")
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculateTotalPrice(self):
+        return (self.price * self.quantity)
+
+
+item1 = Item("Phone", 101, 6)
+print(item1.calculateTotalPrice())
+
+
+
+item2 = Item("Laptop", 1001, 4)
+# Python will automatically print the 
+
+#?adding more attributes to the instances
+item2.hasNumpad = False 
